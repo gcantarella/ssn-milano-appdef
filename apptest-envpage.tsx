2d96e42a477384c5ecddf6d05c98a083294cd7f2
gcantarella <gestioneiagiovanni@gmail.com>
2026-07-10 14:45:21 +0200

    Remove invalid file apptest-envpage.tsx

--- apptest-envpage.tsx
+++
@@ -1,9 +0,0 @@
-Crea un file temporaneo app/test-env/page.tsx:
-tsxexport default function TestEnv() {
-  return (
-    <div>
-      <p>URL: {process.env.NEXT_PUBLIC_SUPABASE_URL}</p>
-      <p>KEY: {process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}</p>
-    </div>
-  )
-}
